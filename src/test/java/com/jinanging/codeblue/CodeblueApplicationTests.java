fe6b42eb1373f84e211d3da2ae7b72b65bf20e20
Jinan <wkdejrrnjs@naver.com>
2026-02-06 20:48:56 +0900

    feat: 초기 프로젝트 설정 및 DB 보안 설정 분리

---
+++ src/test/java/com/jinanging/codeblue/CodeblueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinanging.codeblue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeblueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
